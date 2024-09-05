687cd1057ac644df1b44cf64199faaaec0f05bf6
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2024-09-05 15:01:09 -0400

    Week 1

--- NAA/01-Sep03/01-Hello.cpp
+++ NAA/01-Sep03/01-Hello.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
-
-struct Student {
-   char m_name[41];
-   int m_stno;
-   void print(  ) {
-
-   }
-};
 int main( ) {
    cout << "Hello oop244 naa!!!!" << endl;
    return 0;
